0824dbfbaeae5fccdaf722a705d0502ebd8ddbf3
kentranptit <kentran24797@gmail.com>
2019-02-25 11:03:34 +0700

    Update maps

--- app/Http/Controllers/VisualizationController.php
+++ app/Http/Controllers/VisualizationController.php
@@ -6,66 +6,72 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Employee;
 use App\Models\Leave;
+use DateTime;
 
 class VisualizationController extends Controller
 {
     //
     public function index()
     {
-        $resignedEmp = DB::table('employees')
-                    ->join('leaves','employees.employee_number','=','leaves.employee_number')
-                    ->get();
-        $maleM = 0;
-        $femaleM = 0;
-	    $maleS = 0;
-	    $femaleS = 0; 
-        $maleSper = 0;
-        $femaleSper = 0;
-	    $maleMper=0;
-	    $femaleMper=0;
-        $posdev = 0;
-        $posqa = 0;
-        $pospartime = 0;
-        $posintership = 0;
-        $posother = 0;
-
-        foreach($resignedEmp as $emp) {
-            if ($emp->gender == 'Male' && $emp->marital_status == 'Single')
-                $maleS++;
-            else if ($emp->gender == 'Male' && $emp->marital_status == 'Married')        
-	    	    $maleM++;
-	        else if ($emp->gender == 'Female' && $emp->marital_status =='Single')
-                $femaleS++;
-	        else if ($emp->gender == 'Female' && $emp->marital_status=='Married')
-                $femaleM++;
-            /*
-                if ($emp->last_position == 'DEV')
-                $posdev++;
-            else if ($emp->last_position == 'QA')
-                $posqa++;
-            else if ($emp->last_position == 'Partime' || $emp->last_position == 'partime')
-                $pospartime++;
-            else if ($emp->last_position == 'Intership' || $emp->last_position == 'intership')
-                $posintership++;
-            else $posother++;
-            */
+        $resigned_gender = DB::select("select gender, count(*) as cnt from leaves where not gender = '' group by gender");
+        $arr_gender = [];
+        foreach ($resigned_gender as $emp) {
+            $arr_gender[$emp->gender] = $emp->cnt;
         }
-        $maleSper = round($maleS*100/($maleS + $femaleS + $maleM + $femaleM), 1);
-        $femaleSper = round($femaleS*100/($maleS + $femaleS + $maleM + $femaleM), 1);
-	    $maleMper = round($maleM*100/($maleS + $femaleS + $maleM + $femaleM), 1);
-	    $femaleMper = round($femaleM*100/($maleS + $femaleS + $maleM + $femaleM), 1);
-        //dd($maleSper, $femaleSper, $maleMper, $femaleMper);
-        //dd($maleS, $femaleS, $maleM, $femaleM);
+        $arr_mrStatus = [];
+        $resigned_mrStatus = DB::select("select marital_status, count(*) as cnt from leaves where not marital_status = '' group by marital_status");
+        foreach ($resigned_mrStatus as $emp) {
+            $arr_mrStatus[$emp->marital_status] = $emp->cnt;
+        }
+        $arr_pos = [];
+        $resigned_pos = DB::select("select position, count(*) as cnt from leaves where not position = '' group by position");
+        foreach ($resigned_pos as $emp) {
+            $arr_pos[$emp->position] = $emp->cnt;
+        }
+        $arr_age = [];
+        $resigned_birthday = DB::select("select birthday, probation_date from leaves where not birthday = ''");
+        foreach ($resigned_birthday as $emp) {
+            $dob = date('Y', strtotime($emp->birthday));
+            $probation_date  = date('Y', strtotime($emp->probation_date));
+            $age = $probation_date - $dob;
+            $arr_age[] = $age;
+        }
+        // dd($arr_age);
+        $counter_25 = 0;
+        $counter_25_30 = 0;
+        $counter_30_35 = 0;
+        $counter_35 = 0;
+        foreach ($arr_age as $age) {
+            if ($age <= 25) {
+                $counter_25++;
+            } else if ($age > 25 && $age <= 30) {
+                $counter_25_30++;
+            } else if ($age > 30 && $age <= 35) {
+                $counter_30_35++;
+            } else {
+                $counter_35++;
+            }
+        }
+        // dd($counter_25 . "<br>" . $counter_25_30 . "<br>" . $counter_30_35 . "<br>" . $counter_35);
 
-        return view('employee.index', ['resignedEmp' => $resignedEmp,
-                                       'maleSper' => $maleSper,
-                                       'femaleSper' => $femaleSper,
-				                       'maleMper' => $maleMper,
-				                       'femaleMper' => $femaleMper,
-                                       'posdev' => $posdev,
-                                       'posqa' => $posqa,
-                                       'pospartime' => $pospartime,
-                                       'posintership' => $posintership,
-                                       'posother' => $posother]);
+        $resignedEmp = DB::table('leaves')->get();
+        $arr_add = array();
+        foreach($resignedEmp as $emp) {
+            $add = $emp->address;
+            if($add != "NULL") {
+                array_push($arr_add, $add);
+            }
+        }
+
+        return view('employee.index', [
+                'resigned_gender' => $resigned_gender,
+                'resigned_mrStatus' => $resigned_mrStatus,
+                'resigned_pos' => $resigned_pos,
+                'counter_25' =>$counter_25,
+                'counter_25_30' =>$counter_25_30,
+                'counter_30_35' =>$counter_30_35,
+                'counter_35' =>$counter_35,
+                'arr_add' => $arr_add
+        ]);
 	}
 }
--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -1,65 +1,211 @@
 <html>
 <body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+<script src="http://js.api.here.com/v3/3.0/mapsjs-core.js" type="text/javascript" charset=utf-8></script>
+<script src="http://js.api.here.com/v3/3.0/mapsjs-service.js" type="text/javascript" charset=utf-8></script>
+<script src="http://js.api.here.com/v3/3.0/mapsjs-mapevents.js" type="text/javascript" charset="utf-8"></script>
+<script src="http://js.api.here.com/v3/3.0/mapsjs-ui.js" type="text/javascript" charset="utf-8"></script>
 <h1>Hello, Global PBL Teams!</h1>
 <p>このページはデータベースに接続していくつかのデータを取得、表示するコードを例示するためのサンプルです。<br/>
 詳しくは、Laravelのドキュメントを参照してください。 <a href="http://laravel.jp/">http://laravel.jp/</a></p>
 <p>This page is an example for database connection and getting some records.<br/>
 If you want to learning about Laravel framework so please see a laravel website. <a href="https://laravel.com/">https://laravel.com/</a></p>
 
-<input type="hidden" id="maleS_percent" maleSper="{{$maleSper}}"></input>
-<input type="hidden" id="maleM_percent" maleMper="{{$maleMper}}"></input>
-<input type="hidden" id="femaleS_percent" femaleSper="{{$femaleSper}}"></input>
-<input type="hidden" id="femaleM_percent" femaleMper="{{$femaleMper}}"></input>
-<input type="hidden" id="posdev_amount" posdev="{{$posdev}}"></input>
-<input type="hidden" id="posqa_amount" posqa="{{$posqa}}"></input>
-<input type="hidden" id="pospartime_amount" pospartime="{{$pospartime}}"></input>
-<input type="hidden" id="posintership_amount" posintership="{{$posintership}}"></input>
-<input type="hidden" id="posother_amount" posother="{{$posother}}"></input>
-
-<canvas id="pieChart"></canvas>
+<canvas id="pieGender" width="50" height="20"></canvas>
 <script>
-var maleM = document.getElementById('maleM_percent').getAttribute('maleMper');
-var femaleM = document.getElementById('femaleM_percent').getAttribute('femaleMper');
-var maleS = document.getElementById('maleS_percent').getAttribute('maleSper');
-var femaleS = document.getElementById('femaleS_percent').getAttribute('femaleSper');
-var ctx = document.getElementById('pieChart').getContext('2d');
+var ctx = document.getElementById('pieGender').getContext('2d');
 var chart = new Chart(ctx, {
     type: 'pie',
     data: {
-        labels: ["Male Single", "Male Married", "Female Single", "Female Married"],
+        labels: [@foreach ($resigned_gender as $item) '{{$item->gender}}', @endforeach],
         datasets: [{
-            backgroundColor: ['rgb(99, 132, 255)', 'rgb(255, 99, 132)', 'rgb(160,99,220)', 'rgb(255, 132, 99)'],
-            borderColor: ['rgb(99, 132, 255)', 'rgb(255, 99, 132)', 'rgb(160, 99, 220)', 'rgb(255, 132, 99)'],
-            data: [maleS, maleM, femaleS, femaleM]
+            backgroundColor: ['rgb(255, 99, 132)', 'rgb(99, 132, 255)'],
+            borderColor: ['rgb(255, 99, 132)', 'rgb(99, 132, 255)'],
+            data: [@foreach ($resigned_gender as $item) '{{$item->cnt}}', @endforeach]
         }]
     },
     options: {}
 });
 </script>
 
-<canvas id="barChart"></canvas>
+<canvas id="pieMaritalStatus" width="50" height="20"></canvas>
 <script>
-var posdev = document.getElementById('posdev_amount').getAttribute('posdev');
-var posqa = document.getElementById('posqa_amount').getAttribute('posqa');
-var pospartime = document.getElementById('pospartime_amount').getAttribute('pospartime');
-var posintership = document.getElementById('posintership_amount').getAttribute('posintership');
-var posother = document.getElementById('posother_amount').getAttribute('posother');
-var ctx = document.getElementById('barChart').getContext('2d');
+var ctx = document.getElementById('pieMaritalStatus').getContext('2d');
 var chart = new Chart(ctx, {
-    type: 'bar',
+    type: 'pie',
     data: {
-        labels: ['DEV', 'QA', 'Part-time', 'Internship', 'Others'],
+        labels: [@foreach ($resigned_mrStatus as $item) '{{$item->marital_status}}', @endforeach],
         datasets: [{
-            backgroundColor: ['rgb(99,255,132)', 'rgb(132,255,99)', 'rgb(99,132,255)', 'rgb(132,99,255)', 'rgb(255,132,99)'],
-            borderColor: ['rgb(99,255,132)', 'rgb(132,255,99)', 'rgb(99,132,255)', 'rgb(132,99,255)', 'rgb(255,132,99)'],
-            data: [posdev, posqa, pospartime, posintership, posother]
+            backgroundColor: ['rgb(132, 255, 99)', 'rgb(255, 160, 99)'],
+            borderColor: ['rgb(132, 255, 99)', 'rgb(255, 160, 99)'],
+            data: [@foreach ($resigned_mrStatus as $item) '{{$item->cnt}}', @endforeach]
         }]
     },
-    options: {
-        //barThickness: 'flex'
-    }
+    options: {}
 });
 </script>
+
+<canvas id="doughnutPosition" width="50" height="20"></canvas>
+@php
+$colors = [
+    'rgb(30,0,0)',
+    'rgb(65,0,0)',
+    'rgb(100,0,0)',
+    'rgb(140,0,0)',
+    'rgb(170,0,0)',
+    'rgb(210,0,0)',
+    'rgb(255,0,0)',
+    'rgb(0,30,0)',
+    'rgb(0,65,0)',
+    'rgb(0,100,0)',
+    'rgb(0,140,0)',
+    'rgb(0,170,0)',
+    'rgb(0,210,0)',
+    'rgb(0,255,0)',
+    'rgb(0,0,30)',
+    'rgb(0,0,65)',
+    'rgb(0,0,100)',
+    'rgb(0,0,140)',
+    'rgb(0,0,170)',
+    'rgb(0,0,210)',
+    'rgb(0,0,255)'
+];
+@endphp
+<script>
+var ctx = document.getElementById('doughnutPosition').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: [@foreach ($resigned_pos as $item) '{{$item->position}}', @endforeach],
+        datasets: [{
+            backgroundColor: [@foreach ($colors as $color) '{{$color}}', @endforeach],
+            borderColor: [@foreach ($colors as $color) '{{$color}}', @endforeach],
+            data: [@foreach ($resigned_pos as $item) '{{$item->cnt}}', @endforeach]
+        }]
+    },
+    options: {}
+});
+</script>
+
+<canvas id="lineAge" width="50" height="20"></canvas>
+<script>
+var ctx = document.getElementById('lineAge').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['<25', '25-30', '30-35', '>35'],
+        datasets: [{
+            backgroundColor: ['rgba(255,255,255, 0)'],
+            borderColor: ['rgb(132,255,99)'],
+            data: ['{{$counter_25}}', '{{$counter_25_30}}', '{{$counter_30_35}}', '{{$counter_35}}']
+        }]
+    },
+    options: {}
+});
+</script>
+
+<h3>Map</h3>
+<p id="near"></p>
+<p id="far"></p>
+<div style="width:500px height:500px margin:auto" id='map'></div>
+<div id="geolocation">Geolocation</div>
+<script>
+    var platform = new H.service.Platform({
+        'app_id': 'KTaS5N8kgmop9R0exGB8',
+        'app_code': 'TC7X2f9TI0IHqg5N8M42NA'
+    });
+    console.log(platform);
+    var defaultLayers = platform.createDefaultLayers();
+    var map = new H.Map(
+    document.getElementById('map'), defaultLayers.normal.map,{
+        zoom:13,
+        center:{
+            lat:21.0301527,
+            lng:105.7858207
+        }
+    }
+    );
+    var icon = new H.map.Icon('download.jpg');
+    var circle1 = new H.map.Circle(
+    {
+    lat: 21.0301527,
+    lng: 105.7858207
+    },
+    5000,
+    {
+    style:{
+         strokeColor: 'rgba(75, 171, 73, 1)', // Color of the perimeter
+         lineWidth: 2,
+         fillColor: 'rgba(75, 171, 73, 0.2)'  // Color of the circle
+    }
+    }
+    );
+    map.addObject(circle1);
+    marker = new H.map.Marker({lat: 21.0301527,lng: 105.7858207}, {icon:icon});
+    map.addObject(marker);
+    var mapEvents = new H.mapevents.MapEvents(map);
+    var behavior = new H.mapevents.Behavior(mapEvents);
+    var ui = new H.ui.UI.createDefault(map, defaultLayers);
+    var geoloc = document.getElementById('geolocation');
+    function distance(lat1, lon1, lat2, lon2) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+                return 0;
+                }
+                else {
+                var radlat1 = Math.PI * lat1/180;
+                var radlat2 = Math.PI * lat2/180;
+                var theta = lon1-lon2;
+                var radtheta = Math.PI * theta/180;
+                var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(rad\
+theta);
+                if (dist > 1) {
+                dist = 1;
+                }
+                dist = Math.acos(dist);
+                dist = dist * 180/Math.PI;
+                dist = dist * 60 * 1.1515;
+                dist = dist * 1.609344;
+                return dist;
+                }
+      }
+      var far = 0;
+      var near = 0;
+    </script>
+@foreach ($arr_add as $add)
+        <script>
+            var addText = <?php echo json_encode($add);?>;
+            var geocodingParams = {
+            searchText: addText
+            };
+            // Define a callback function to process the geocoding response:
+            var onResult = function(result) {
+            var locations = result.Response.View[0].Result,
+            position,
+            marker;
+            position = {
+            lat: locations[0].Location.DisplayPosition.Latitude,
+            lng: locations[0].Location.DisplayPosition.Longitude
+            };
+            marker = new H.map.Marker(position);
+            map.addObject(marker);
+            if(distance(position.lat,position.lng,21.0301527,105.7858207)<5){
+                near ++;
+            }
+            else {
+                far ++;
+            }
+            console.log(far +" "+ near);
+            document.getElementById("near").innerHTML = "Number of people who resigned live within 5km: ~"+ near;
+            document.getElementById("far").innerHTML = "Number of people who resigned live beyond 5km: ~" +far;
+            };
+            var geocoder = platform.getGeocodingService();
+            geocoder.geocode(geocodingParams, onResult, function(e) {
+            alert(e);
+            })
+        </script>
+    </div>
+@endforeach
+
+
 </body>
 </html>
